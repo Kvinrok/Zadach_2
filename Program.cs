aaf490ea3df78bc85ab30d66b76dce6972c987d6
Alexadr <sanz76@.mail.ru>
2022-12-03 14:20:17 +0300

    DZ_2

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+            int num_A = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число: ");
+            int num_B = Convert.ToInt32(Console.ReadLine());
+
+            if (num_A > num_B)
+            {
+                Console.WriteLine("Первое число большее " + num_A + "  второе число меньшее " + num_B);
+            }
+            else
+            {
+                Console.WriteLine("Второе число большее " + num_B + " первое число меньшее " + num_A);
+            }
